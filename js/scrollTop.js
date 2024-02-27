27c7d044824d90c958cd7265a568084b27955b8f
Gyuri Kim <133613660+solcasa@users.noreply.github.com>
2024-02-27 16:29:18 +0900

    현대엘리베이터 다시해보기
    px 단위, 1200 으로 함.

---
+++ js/scrollTop.js
@@ -0,0 +1,17 @@
+const TO_TOP_BUTTON = document.querySelector('.to_top');
+
+TO_TOP_BUTTON.addEventListener('click', () => {
+    gsap.to(window, {
+        scrollTo: 0,
+        duration: 1,
+    });
+});
+
+window.addEventListener('scroll', () => {
+    const SCROLL_TOP = window.scrollY;
+    SCROLL_TOP > 1000
+        ?
+        TO_TOP_BUTTON.classList.add('on')
+        :
+        TO_TOP_BUTTON.classList.remove('on')
+});
